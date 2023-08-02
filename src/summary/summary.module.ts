c1a5977dd7e06df6fc00f79c9edf04faa689ba83
ryotarofr <ryoryo.fr0608@icloud.com>
2023-08-02 10:51:54 +0900

    ➕ inport ReportModule

--- src/summary/summary.module.ts
+++ src/summary/summary.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SummaryController } from './summary.controller';
+import { SummaryService } from './summary.service';
+import { ReportService } from 'src/report/report.service';
+import { ReportModule } from 'src/report/report.module';
 
 @Module({
-  controllers: [SummaryController]
+  imports: [ReportModule],
+  controllers: [SummaryController],
+  providers: [SummaryService]
 })
-export class SummaryModule {}
+export class SummaryModule { }
